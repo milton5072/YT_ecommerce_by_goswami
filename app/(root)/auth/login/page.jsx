8f8ca82e979af01f50557c6da332bb73eee75929
milton <miltonkazi2004@gmail.com>
2025-11-11 09:59:41 +0600

    loging from

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,71 +1,104 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import {
 	Card,
 	CardAction,
 	CardContent,
 	CardFooter,
 } from "@/components/ui/card";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-const loginPage = () => {
+import { zSchema } from "@/lib/zodSchema";
+
+const formSchema = zSchema.pick({
+	email: true,
+	password: true,
+});
+
+const LoginPage = () => {
+	// useForm সবসময় component এর ভিতরে কল করতে হবে
+	const form = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	//submit handler
+	const handleLoginSubmit = async (value) => {
+		console.log("Submitted:", value);
+	};
+
 	return (
 		<Card className="w-full max-w-sm">
 			<CardContent>
 				<p className="text-center text-2xl font-semibold">Logo</p>
 				<p className="text-center mt-4">Log in to your account!</p>
-				<form>
-					<div className="flex flex-col gap-6">
-						<div className="grid gap-2">
-							<Label
-								htmlFor="email"
-								className="text-sm text-gray-400"
-							>
-								Email
-							</Label>
-							<Input
-								id="email"
-								type="email"
-								placeholder="m@example.com"
-								required
-							/>
-						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label
-									htmlFor="password"
-									className="text-sm text-gray-400"
-								>
-									Password
-								</Label>
-								<a
-									href="#"
-									className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-								>
-									Forgot your password?
-								</a>
-							</div>
-							<Input
-								id="password"
-								type="password"
-								required
-							/>
-						</div>
-					</div>
-				</form>
+
+				{/* form wrapper */}
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(handleLoginSubmit)}
+						className="space-y-4"
+					>
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											type="email"
+											placeholder="example@gmail.com"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											placeholder="•••••••"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<Button
+							type="submit"
+							className="w-full"
+						>
+							Login
+						</Button>
+					</form>
+				</Form>
 			</CardContent>
+
 			<CardFooter className="flex-col gap-2">
-				<Button
-					type="submit"
-					className="w-full"
-				>
-					Login
-				</Button>
-				<Button
-					variant="outline"
-					className="w-full"
-				>
-					Login with Google
-				</Button>
 				<CardAction>
 					<Button variant="link">Sign Up</Button>
 				</CardAction>
@@ -73,4 +106,5 @@ const loginPage = () => {
 		</Card>
 	);
 };
-export default loginPage;
+
+export default LoginPage;
