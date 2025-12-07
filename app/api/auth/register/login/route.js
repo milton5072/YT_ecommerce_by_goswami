63fe90fa28d81afb4e7d492b178c9021c288affe
milton <miltonkazi2004@gmail.com>
2025-12-07 10:20:13 +0600

    asad

---
+++ app/api/auth/register/login/route.js
@@ -0,0 +1,62 @@
+import z from "zod";
+import { connectDB } from "../../../../../lib/databaseConnection";
+import { catchError } from "../../../../../lib/helperFunction";
+import { zSchema } from "../../../../../lib/zodSchema";
+import { z } from "zod";
+export async function POST(request) {
+	try {
+		await connectDB();
+		const payload = await request.json();
+		const validationSchema = zSchema
+			.pick({
+				email: true,
+			})
+			.extend({ password: z.string() });
+		const validatedData = validationSchema.safeParse(payload);
+		if (!validatedData.success) {
+			return response(
+				false,
+				401,
+				"Invalid or missing input field.",
+				validatedData.error
+			);
+		}
+		const { email, password } = validatedData.data;
+		//get user data
+		const getUser = await UserModel.findOne({ email });
+		if (!getUser) {
+			return response(false, 400, "Invalid login credentials.");
+		}
+		//resend verification email if not verified
+		if (!getUser.isEmailVerified) {
+			const secret = new TextEncoder().encode(process.env.SECRET_KEY);
+			const token = await new SignJWT({
+				userId: newRegistration._id.toString(),
+			})
+				.setIssuedAt()
+				.setExpirationTime("1h")
+				.setProtectedHeader({ alg: "HS256" })
+				.sign(secret);
+			await sendMail(
+				"Email Verification Request from Kazi Asad",
+				email,
+				emailVerificationLink(
+					`${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-email/token?token=${token}`
+				)
+			);
+			return response(
+				false,
+				401,
+				"Please verify your email address. A new verification email has been sent to your email."
+			);
+		}
+		//compare password
+		const isPasswordVerified = await getUser.comparePassword(password);
+		if (!isPasswordVerified) {
+			return response(false, 400, "Invalid login credentials.");
+		}
+		// otp generation
+	} catch (error) {
+		return catchError(error, "Login failed.");
+	}
+}
