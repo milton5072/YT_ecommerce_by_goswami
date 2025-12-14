00ecc4feca49c77768150cf6034032af241cb5b9
milton <miltonkazi2004@gmail.com>
2025-12-14 09:34:44 +0600

    dashboard

--- app/(root)/admin/dashboard/layout.jsx
+++ app/(root)/admin/dashboard/layout.jsx
@@ -1,16 +1,15 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
-import AppSidebar from "@/components/application/Admin/AppSidebar"
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/application/Admin/AppSidebar";
 
-
-const layout = ({ children })=> {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        {/* <SidebarTrigger /> */}
-        {children}
-      </main>
-    </SidebarProvider>
-  )
-}
-export default layout
+const layout = ({ children }) => {
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<main>
+				{/* <SidebarTrigger /> */}
+				{children}
+			</main>
+		</SidebarProvider>
+	);
+};
+export default layout;
--- components/application/Admin/AppSidebar.jsx
+++ components/application/Admin/AppSidebar.jsx
@@ -1,23 +1,74 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { AiOutlineDashboard } from "react-icons/ai";
+import adminAppSidebarMenu from "../../../lib/adminSidebarMenu";
+import Link from "next/link";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { LuChevronRight } from "react-icons/lu";
 
 const AppSidebar = () => {
-    return <Sidebar>
-      <div className="flex justify-between items-center font-bold dark:text-white px-4">Logo
-        <button type="button">X</button>
-      </div>
-      <SidebarContent>
-        
-      </SidebarContent>
-    </Sidebar>;
+	return (
+		<Sidebar>
+			<div className="flex justify-between items-center font-bold dark:text-white px-4 border-b py-3">
+				Logo
+				<button type="button">X</button>
+			</div>
+			<SidebarContent>
+				<SidebarMenu>
+					{adminAppSidebarMenu.map((menu, index) => (
+						<Collapsible
+							key={index}
+							className="group/collapsible"
+						>
+							<SidebarMenuItem>
+								<CollapsibleTrigger asChild>
+									<SidebarMenuButton asChild>
+										<Link href={menu?.url}>
+											<menu.icon />
+											<span>{menu.title}</span>
+											{menu?.submenu && menu.submenu.length > 0 && (
+												<LuChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+											)}
+										</Link>
+									</SidebarMenuButton>
+								</CollapsibleTrigger>
+								{menu?.submenu && menu.submenu.length > 0 && (
+									<CollapsibleContent>
+										<SidebarGroup className="mt-1 mb-2">
+											<SidebarGroupContent>
+												{menu.submenu.map((subMenu, subIndex) => (
+													<SidebarGroupLabel key={subIndex}>
+														<Link
+															href={subMenu.url}
+															className="pl-10"
+														>
+															{subMenu.title}
+														</Link>
+													</SidebarGroupLabel>
+												))}
+											</SidebarGroupContent>
+										</SidebarGroup>
+									</CollapsibleContent>
+								)}
+							</SidebarMenuItem>
+						</Collapsible>
+					))}
+				</SidebarMenu>
+			</SidebarContent>
+		</Sidebar>
+	);
 };
 
 export default AppSidebar;
